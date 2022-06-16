b689ee28cce0598cabd1f95e15ad62e702083367
nobody418 <bryaneliecer@hotmail.com>
2022-06-15 19:28:06 -0500

    Se prueba el codigo para leer el sensor MPU

--- RPendulo/RPendulo.ino
+++ RPendulo/RPendulo.ino
@@ -1,12 +1,29 @@
-#define LM1 13
-#define LM2 15
-#define RM1 7
-#define RM2 6
-#define PWML 14
-#define PWMR 12
+#include <Arduino.h>    //Incluye librerias para arduino
+#include <Wire.h>       //Incluye libreria para I2C
+
+#define MPU 0x69        //Direccion I2C de la IMU
+//Radios de conversion
+#define A_R 16384.0 // 32768/2
+#define G_R 131.0 // 32768/250
+//Conversion de radianes a grados 180/PI
+#define RAD_A_DEG = 57.295779
+//MPU-6050 da los valores en enteros de 16 bits
+//Valores RAW
+int16_t AcX, AcY, AcZ;
+//Angulos
+float Acc[2];
+String valores;
+
+
+//DEFINE PINES DRIVER de MOTORES L298N
+#define LM1 0    //=D3   //IN1
+#define LM2 2    //=D4   //IN2
+#define RM1 13   //=D7   //IN3
+#define RM2 15   //=D8   //IN4
+#define PWML 14  //=D5   //ENA
+#define PWMR 12  //=D6   //ENB
 
 int v = 0;
-bool sb = 0;
 
 void setup() {
   pinMode(LM1, OUTPUT);
@@ -16,28 +33,29 @@ void setup() {
   pinMode(PWML, OUTPUT);
   pinMode(PWMR, OUTPUT);
 
-  //Serial.begin(9600);
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  //Serial.println(v);
-  mov(v);
-  delay(300);
-  if (v < 255 && !sb) {
-    v += 90;
-  }
-  if (v >= 255) {
-    sb = 1;
-  }
-  if (v > -255 && sb) {
-    v -= 90;
-  }
-  if (v <= -255) {
-    sb = 0;
-  }
+  //Serial.println(estado);
+  delay(400);
+  leeMPU();
+
+//  mov(v);
+//  
+//  v += 90;
+//  if (v >= 255) {
+//    v = -255;
+//  }
 }
 
-void mov(int v) {
+void mov(int v) { //Movimiento del robot
   velMot(abs(v));
   if (v > 255) {
     v = 255;
@@ -46,27 +64,60 @@ void mov(int v) {
     v = -255;
   }
   if (v >= 0) {
-    motorAv();
+    motorFW();
   } else {
-    motorRe();
+    motorBW();
   }
 }
 
-void velMot(int v) {
+
+void velMot(int v) { //Velocidad de motores
   analogWrite(PWML, v);
   analogWrite(PWMR, v);
 }
 
-void motorAv() {
+void motorFW() { //Avance de motores
   digitalWrite(RM1, 1);
   digitalWrite(RM2, 0);
   digitalWrite(LM1, 1);
   digitalWrite(LM2, 0);
 }
 
-void motorRe() {
+void motorBW() { //Retroceso de motores
   digitalWrite(RM1, 0);
   digitalWrite(RM2, 1);
   digitalWrite(LM1, 0);
   digitalWrite(LM2, 1);
+}
+
+void motorStop() { //Paro de motores
+  digitalWrite(RM1, 0);
+  digitalWrite(RM2, 0);
+  digitalWrite(LM1, 0);
+  digitalWrite(LM2, 0);
+}
+
+void leeMPU() {
+  //Leer los valores del Acelerometro de la IMU
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B); //Pedir el registro 0x3B - corresponde al AcX
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 6, true); //A partir del 0x3B, se piden 6 registros
+  AcX = Wire.read() << 8 | Wire.read(); //Cada valor ocupa 2 registros
+  AcY = Wire.read() << 8 | Wire.read();
+  AcZ = Wire.read() << 8 | Wire.read();
+
+  //Calculo de los angulos Y, X respectivamente, con la formula de la tangente.
+  //Acc2[1] = atan(-1*(AcX2/A_R2)/sqrt(pow((AcY2/A_R2),2) + pow((AcZ2/A_R2),2)))*RAD_TO_DEG;
+  //Acc2[0] = atan((AcY2/A_R2)/sqrt(pow((AcX2/A_R2),2) + pow((AcZ2/A_R2),2)))*RAD_TO_DEG;
+
+  Acc[0] = atan((AcY) / sqrt(pow((AcX), 2) + pow((AcZ), 2)));
+
+
+  valores = "90, " + String(Acc[0]) + "," ;
+  Serial.println("x="+String(AcX)+", y="+String(AcY)+", z="+String(AcZ));
+  Serial.println("Angulos MPU");
+  Serial.println(valores);
+
+  delay(10);
 }
